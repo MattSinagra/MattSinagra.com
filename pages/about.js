5c2d9c80a5b3dee56d23d555a8a149d380e130d4
Matt Sinagra <matthew.sinagra@gmail.com>
2020-02-01 21:21:28 +1000

    Added Collection

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Head from "next/head"
+import { Component } from 'react'
+
+
+import React from 'react'
+
+function About() {
+    return (
+      <React.Fragment>
+          <p>This is the about page</p>
+      </React.Fragment>  
+    )
+}
+
+export default About
